43aba6d43e9665a73d3942df0285e833f0bda8bc
nonqme <wissem.missary@gmail.com>
2022-02-02 00:36:00 +0100

    Dessine le fond avec un système de grille  en fonction des coordonnées

--- World.js
+++ World.js
@@ -3,19 +3,24 @@
 export default class World {
     constructor(canvas) {
         this.gameWidth = canvas.width;
-        this.gameHeigth = canvas.height;
-        this.tileWidth = 32;
-        this.tileHeight = 32; 
+        this.gameHeight = canvas.height;
+        this.tileWidth = 30;
+        this.tileHeight = 30; 
         this.x = 0;
         this.y = this.gameHeigth - this.tileHeight;
-        this.image = document.querySelector('.background')
+        this.grid = [];
     }
 
     draw(context) {
-        context.fillStyle = 'green';
-        for (this.y = this.gameHeigth; this.y >= 0; this.y -= this.tileHeight) {
-            console.log(this.y)
+        context.fillStyle = 'white'
+        for (this.y = 0; this.y < this.gameHeight; this.y += this.tileHeight) {
             for (this.x = 0; this.x < this.gameWidth; this.x += this.tileWidth) {
+                console.log(this.y)
+                if (this.y === this.gameHeight - this.tileHeight) {
+                    context.fillStyle = 'green';
+                }else {
+                    context.fillStyle = 'blue'
+                }
                 context.fillRect(this.x, this.y, this.tileWidth, this.tileHeight)
             }
         }
